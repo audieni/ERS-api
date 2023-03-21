5cf1e1b3e1b82ce5d991de61017346ec2f6b82c9
Audie Ni <111254997+tasukaru@users.noreply.github.com>
2023-03-21 10:53:18 -0400

    added boilerplate code

---
+++ src/test/java/com/audieni/ers/ErsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audieni.ers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
